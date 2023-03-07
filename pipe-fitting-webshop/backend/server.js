36374ec64f51efe739c739f9e0aa9432c947c1f6
Jani <ambja1983@gmail.com>
2023-03-07 17:29:32 +0100

    03.07

---
+++ pipe-fitting-webshop/backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+
+//* endpoints
+app.post('/login', (req, res) => {
+    console.log(req.body);
+});
+
+app.post('/registration', (req, res) => {
+    console.log(req.body);
+})
+
+app.put('/update_account', (req, res) => {
+    console.log(req.body);
+})
+
+//* server start
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})
