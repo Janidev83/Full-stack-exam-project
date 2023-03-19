b658e82282f160ec3527a90ba7ae71ef7edd5115
Jani <ambja1983@gmail.com>
2023-03-19 17:31:45 +0100

    03.19

--- pipe-fitting-webshop/backend/server.js
+++ pipe-fitting-webshop/backend/server.js
@@ -33,6 +33,11 @@ app.use((req, res) => {
     res.send('Page not found!');
 })
 
+//* error-handling
+app.use((err, req, res, next) => {
+    res.status(err.statusCode).json({error: err.message});
+})
+
 //* server start
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
