c02b89343cef643df142577352f066c8c26c08c9
Jani <ambja1983@gmail.com>
2023-04-08 18:20:05 +0200

    04.08

--- pipe-fitting-webshop/backend/src/controller/product/product.repository.js
+++ pipe-fitting-webshop/backend/src/controller/product/product.repository.js
@@ -5,4 +5,4 @@ exports.find = async (volume) => Product.find().skip(volume).limit(6);
 
 //! Ha jól működik a products, nem keveredik össze, akkor ezeket kivenni és a db.json-t is!
 const insertData = async DB => Product.insertMany(DB);
-insertData(DB.products);
+//insertData(DB.products);
