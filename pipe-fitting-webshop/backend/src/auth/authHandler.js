82d255e2d67275bec506e88c83fa8fefa2f926f5
Jani <ambja1983@gmail.com>
2023-04-08 19:51:30 +0200

    04.08

--- pipe-fitting-webshop/backend/src/auth/authHandler.js
+++ pipe-fitting-webshop/backend/src/auth/authHandler.js
@@ -57,7 +57,7 @@ exports.login = async (req, res, next) => {
 
 exports.refresh = async (req, res, next) => {
     try {
-        const {refreshToken} = req.body;//* Fontos a bejövő body elnevezés: refreshToken: 'seggg3526' pl.
+        const {refreshToken} = req.body;
         if(!refreshToken) {
             return next(new createError.Unauthorized('Unauthorized request'));
         }
