db517a6a7973f5cdfcce5c6789a780966a87c201
Jani <ambja1983@gmail.com>
2023-04-10 20:07:35 +0200

    integrációs tesztek

--- pipe-fitting-webshop/backend/src/server.test.js
+++ pipe-fitting-webshop/backend/src/server.test.js
@@ -64,6 +64,11 @@ describe('REST API integration tests', ()=> {
               price: 710,
               imageUrl: "http://localhost:3000/images/polyfusion_coupler.jpg"
             }
+        ],
+        tokens: [
+            {
+                refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDI5YTAxY2I2NWZkYTE4ZDFkYjQzNmYiLCJsYXN0TmFtZSI6IkFtYnJ1cyIsImZpcnN0TmFtZSI6IkrDoW5vcyIsImFkZHJlc3MiOiIxMDMxIEJ1ZGFwZXN0LCBDc8OzbmFraMOheiB1dGNhIDkuIiwiZW1haWwiOiJhbWJqYTE5ODNAZ21haWwuY29tIiwib3JkZXJzIjpbIjY0MzE1Njc4NTBjNzNiZmNmMTM4M2FkNyIsIjY0MzE2YjM5YWE5MmU4MzJiNGJhMjU3YyIsIjY0MzE3NzEyYWE5MmU4MzJiNGJhMjVhYSIsIjY0MzE4MmVhNzllOTYzYjFiN2VhODNlMSIsIjY0MzJmNmNkZTcxYWMwZGRlOWQwMjk0OCIsIjY0MzMyNTFiZTZkYTJlZGQzNGQwZWMxNSIsIjY0MzMyNzZiODg2NGJjYWNmYmFmYTM2MSIsIjY0MzQyZTg3MjAzNWUzYWFlZTc0NjMzNCIsIjY0MzQzYmE4ZTJkYjkwZjk5ZjM3ZjkzMyJdLCJpYXQiOjE2ODExNDY3NDR9.69JJSvbX8cTnUoNjHZvtNa076pTa8A__8si7vk8nBKg"
+            }
         ]
     };
 
@@ -88,6 +93,7 @@ describe('REST API integration tests', ()=> {
         const response = await supertest(app).post('/api/login').send(loginData);
         expect(response.statusCode).toBe(200);
         expect(response.body).toHaveProperty('accessToken');
+        expect(response.body.accessToken).toBeTruthy();
         expect(response.body).toHaveProperty('customer');
         expect(response.body.customer._id).toBeTruthy();
         expect(response.body.customer.lastName).toBe(insertData.customers[0].lastName);
@@ -96,10 +102,30 @@ describe('REST API integration tests', ()=> {
         expect(response.body.customer.email).toBe(insertData.customers[0].email);
         expect(response.body.customer.orders).toBeTruthy();
         expect(response.body).toHaveProperty('refreshToken');
+        expect(response.body.refreshToken).toBeTruthy();
     });
 
     test('POST /refresh endpoint with valid refreshtoken', async () => {
-        
+        await Token.insertMany(insertData.tokens);
+        const token = {
+            refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDI5YTAxY2I2NWZkYTE4ZDFkYjQzNmYiLCJsYXN0TmFtZSI6IkFtYnJ1cyIsImZpcnN0TmFtZSI6IkrDoW5vcyIsImFkZHJlc3MiOiIxMDMxIEJ1ZGFwZXN0LCBDc8OzbmFraMOheiB1dGNhIDkuIiwiZW1haWwiOiJhbWJqYTE5ODNAZ21haWwuY29tIiwib3JkZXJzIjpbIjY0MzE1Njc4NTBjNzNiZmNmMTM4M2FkNyIsIjY0MzE2YjM5YWE5MmU4MzJiNGJhMjU3YyIsIjY0MzE3NzEyYWE5MmU4MzJiNGJhMjVhYSIsIjY0MzE4MmVhNzllOTYzYjFiN2VhODNlMSIsIjY0MzJmNmNkZTcxYWMwZGRlOWQwMjk0OCIsIjY0MzMyNTFiZTZkYTJlZGQzNGQwZWMxNSIsIjY0MzMyNzZiODg2NGJjYWNmYmFmYTM2MSIsIjY0MzQyZTg3MjAzNWUzYWFlZTc0NjMzNCIsIjY0MzQzYmE4ZTJkYjkwZjk5ZjM3ZjkzMyJdLCJpYXQiOjE2ODExNDY3NDR9.69JJSvbX8cTnUoNjHZvtNa076pTa8A__8si7vk8nBKg"
+        };
+
+        const response = await supertest(app).post('/api/refresh').send(token);
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('accessToken');
+        expect(response.body.accessToken).toBeTruthy();
+    });
+
+    test('POST /logout endpoint with valid refreshtoken', async () => {
+        await Token.insertMany(insertData.tokens);
+        const token = {
+            refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDI5YTAxY2I2NWZkYTE4ZDFkYjQzNmYiLCJsYXN0TmFtZSI6IkFtYnJ1cyIsImZpcnN0TmFtZSI6IkrDoW5vcyIsImFkZHJlc3MiOiIxMDMxIEJ1ZGFwZXN0LCBDc8OzbmFraMOheiB1dGNhIDkuIiwiZW1haWwiOiJhbWJqYTE5ODNAZ21haWwuY29tIiwib3JkZXJzIjpbIjY0MzE1Njc4NTBjNzNiZmNmMTM4M2FkNyIsIjY0MzE2YjM5YWE5MmU4MzJiNGJhMjU3YyIsIjY0MzE3NzEyYWE5MmU4MzJiNGJhMjVhYSIsIjY0MzE4MmVhNzllOTYzYjFiN2VhODNlMSIsIjY0MzJmNmNkZTcxYWMwZGRlOWQwMjk0OCIsIjY0MzMyNTFiZTZkYTJlZGQzNGQwZWMxNSIsIjY0MzMyNzZiODg2NGJjYWNmYmFmYTM2MSIsIjY0MzQyZTg3MjAzNWUzYWFlZTc0NjMzNCIsIjY0MzQzYmE4ZTJkYjkwZjk5ZjM3ZjkzMyJdLCJpYXQiOjE2ODExNDY3NDR9.69JJSvbX8cTnUoNjHZvtNa076pTa8A__8si7vk8nBKg"
+        };
+
+        const response = await supertest(app).post('/api/logout').send(token);
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({});
     });
 
     test('POST /registration endpoint with valid data', async () => {
@@ -114,5 +140,35 @@ describe('REST API integration tests', ()=> {
         const response = await supertest(app).post('/api/registration').send(newCustomer);
         expect(response.statusCode).toBe(201);
         expect(response.body).toEqual({});
+    });
+
+    test('PUT /customer/:id endpoint with valid id and data', async () => {
+        const savedCustomers = await Customer.insertMany(insertData.customers);
+        const loginResponse = await supertest(app).post('/api/login')
+        .send({
+            email: "kbela@gmail.com",
+            password: "belus25"
+        });
+        ACCESS_TOKEN = loginResponse.body.accessToken;
+        REFRESH_TOKEN = loginResponse.body.refreshToken;
+
+        const updateData = {
+            lastName: "Kómár",
+            firstName: "János",
+            address: "Csónakház utca 9.",
+            email: "kbela@gmail.com",
+            password: "janika123"
+        };
+
+        const response = await supertest(app).put(`/api/customer/${savedCustomers[0]._id.toString()}`)
+        .set('Authorization', `Bearer ${ACCESS_TOKEN}`).send(updateData);
+
+        expect(response.statusCode).toBe(201);
+        expect(response.body._id).toBe(savedCustomers[0]._id.toString());
+        expect(response.body.lastName).toBe(savedCustomers[0].lastName);
+        expect(response.body.firstName).toBe(updateData.firstName);
+        expect(response.body.address).toBe(updateData.address);
+        expect(response.body.email).toBe(savedCustomers[0].email);
+        expect(response.body.orders).toEqual(savedCustomers[0].orders);
     });
 });
